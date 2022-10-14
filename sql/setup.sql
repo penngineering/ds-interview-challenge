c0c018330bcaf6bae5e2a44d3c21c714527e7054
camrmarquis <camrmarquis@gmail.com>
2022-10-14 14:12:56 -0400

    add data

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,47 @@
-CREATE TABLE iris (
-  Id integer PRIMARY KEY,
-  SepalLengthCM float,
-  SepalWidthCM float,
-  PetalLengthCm float,
-  PetalWidthCm float,
-  Species varchar
-)
+CREATE TABLE schedule (
+  event_id varchar PRIMARY KEY,
+  home_team varchar,
+  away_team varchar,
+  game_start_time timestamp,
+  week_start timestamp,
+  week_of_season float
+);
 
-COPY iris FROM '/var/lib/postgresql/data/Iris.csv' csv header;
+COPY schedule FROM '/var/lib/postgresql/data/schedule.csv' csv header;
+
+CREATE TABLE teams (
+  team_name varchar PRIMARY KEY,
+  location varchar
+);
+
+COPY teams FROM '/var/lib/postgresql/data/teams.csv' csv header;
+
+CREATE TABLE users (
+  user_number varchar PRIMARY KEY,
+  age float,
+  registration_date timestamp,
+  location varchar
+);
+
+COPY users FROM '/var/lib/postgresql/data/users.csv' csv header;
+
+CREATE TABLE wagers (
+  bet_id integer PRIMARY KEY,
+  user_umber varchar,
+  event_id varchar,
+  bet_offer_type varchar,
+  wager_amount float,
+  outcome_decimal_odds float,
+  outcome_american_odds float,
+  bet_placed_time timestamp,
+  bet_status varchar,
+  bet_result varchar,
+  payout float
+);
+
+COPY wagers FROM '/var/lib/postgresql/data/wagers.csv' csv header;
+
+UPDATE wagers SET wager_amount = ROUND(CAST(wager_amount as numeric), 2);
+
+UPDATE wagers SET payout = ROUND(CAST(payout as numeric), 2)
 
